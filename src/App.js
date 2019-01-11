ab2e711eaa7cafa20fd83a3edc36c9ec33f4c460
Neelam Chaurasia <chaurasia.neelam@gmail.com>
2019-01-11 16:20:34 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./component/navbar";
+import Counters from "./component/counters";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 }
+    ]
+  };
+  handleIncrement = counter => {
+    console.log("In Incrementcount");
+    const counters = [...this.state.counters];
+    const index = this.state.counters.indexOf(counter);
+    counters[index] = { ...counter };
+    console.log(counters[index]);
+    counters[index].value++;
+    this.setState({ counters });
+  };
+  handleDelete = counterId => {
+    console.log("Event Handle..", counterId);
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+
+    this.setState({ counters });
+  };
+  handleReset = () => {
+    console.log("In Reset");
+    const counters = this.state.counters.map(c => {
+      c.value = 0;
+      return c;
+    });
+    return this.setState({ counters });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <React.Fragment>
+        <Navbar
+          totlaCounter={this.state.counters.filter(c => c.value > 0).length}
+        />
+        <main className="container">
+          <Counters
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counters={this.state.counters}
+          />
+        </main>
+      </React.Fragment>
     );
   }
 }
